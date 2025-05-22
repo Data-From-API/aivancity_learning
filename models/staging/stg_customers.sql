cfc9cb1ca1bf5fc290ed2ed829b34c1a61bde84c
eric@datafromapi.com <eric@datafromapi.com>
2025-05-22 16:53:32 +0000

    first init of the project and creating the first model for customers

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,22 @@
+{{ 
+    config(
+        materialized="table",
+        partition_by = {
+            "field": "signup_date",
+            "data_type": "date",
+            "granularity": "day"
+        },
+        cluster_by = ["loyalty_level"],
+    ) 
+}}
+
+select
+    customer_id,
+    first_name,
+    last_name,
+    concat(first_name, ' ', last_name) as fullname,
+    gender,
+    is_loyalty_member,
+    loyalty_level,
+    date(signup_date) as signup_date
+from {{ source('sources_tables','dim_customers') }}
